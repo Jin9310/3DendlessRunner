110f73f9d2e6eeff83ece60248cb8be69a854877
Jindrich <stejskalj@hotmail.com>
2021-12-29 22:55:18 +0100

    generation script and new assets

--- 3DendlessRunner/Assets/LevelGenerationTrigger.cs
+++ 3DendlessRunner/Assets/LevelGenerationTrigger.cs
@@ -8,12 +8,20 @@ public class LevelGenerationTrigger : MonoBehaviour
     [SerializeField] private float xPosition;
     [SerializeField] private float yPosition;
 
+    [SerializeField] private int rand = 0;
+    [SerializeField] private int repeat = 0;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            var rand = Random.Range(0, levelCollection.Length);
+            rand = Random.Range(0, levelCollection.Length);
+            while (rand == repeat)
+            {
+                rand = Random.Range(0, levelCollection.Length);
+            }
             Instantiate(levelCollection[rand], new Vector3(transform.position.x + xPosition, transform.position.y + yPosition, transform.position.z), Quaternion.identity);
+            repeat = rand;
         }
     }
 }
